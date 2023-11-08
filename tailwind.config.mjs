32e724344aa13512340711ece04c74a756245c6f
Peyton <peyton@geovision.com.tw>
2023-11-08 18:01:46 +0800

    base header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+      current: 'currentColor',
+			black: '#282828',
+			white: '#D9D9D9',
+		},
 		extend: {},
 	},
 	plugins: [],
